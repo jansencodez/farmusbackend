bd2bd0fe4bb8b8c9f550224d2f818dbe7196aac6
jansencodez <nyamwecliff@gmail.com>
2024-09-27 18:21:32 +0300

    minor changes

--- api/hello.js
+++ api/hello.js
@@ -1,3 +1,3 @@
-export default  function handler(req, res){
-  res.status(200).json({message: "hello form me"})
+export default function handler(req, res) {
+  res.status(200).json({ message: 'Hello, world!' });
 }
