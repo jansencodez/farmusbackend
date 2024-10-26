ba6f863b7d43fe3a41c0d1c57c60664f29ae2477
jansencodez <nyamwecliff@gmail.com>
2024-10-26 17:52:41 +0300

    added carts

--- models/User.js
+++ models/User.js
@@ -8,16 +8,27 @@ const TransactionSchema = new mongoose.Schema({
   description: { type: String },
 });
 
+const CartItemSchema = new mongoose.Schema({
+  product: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "Product",
+    required: true,
+  },
+  quantity: { type: Number, default: 1 },
+  price: { type: mongoose.Schema.Types.Decimal128, required: true }, // price at time of adding to cart
+});
+
 const UserSchema = new mongoose.Schema({
   name: { type: String, required: true },
   email: { type: String, required: true, unique: true },
   password: { type: String, required: true },
   resetPasswordToken: String,
   resetPasswordExpires: Date,
-  profilePicture: { type: String }, // Add this field to store the path to the profile picture
+  profilePicture: { type: String },
   products: [{ type: mongoose.Schema.Types.ObjectId, ref: "Product" }],
   walletBalance: { type: mongoose.Schema.Types.Decimal128, default: 0.0 },
   transactionHistory: [TransactionSchema],
+  cart: [CartItemSchema], // Added cart field to store items in the user's cart
 });
 
 UserSchema.pre("save", async function (next) {
